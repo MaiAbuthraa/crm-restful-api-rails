8553a4156bc3b194c26ac8901044c8db7e91f038
MaiAbuthraa <mai.abuthraa@gmail.com>
2020-11-16 11:29:43 +0200

    Create Custom Failure APP

---
+++ lib/custom_devise_failure_app.rb
@@ -0,0 +1,10 @@
+class CustomDeviseFailureApp < Devise::FailureApp
+  def http_auth_body
+    return super unless request_format == :json
+
+    {
+      sucess: false,
+      message: i18n_message
+    }.to_json
+  end
+end
