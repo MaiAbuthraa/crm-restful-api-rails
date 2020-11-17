039771d88f46fb149d0140d7177973d63472b2e7
MaiAbuthraa <mai.abuthraa@gmail.com>
2020-11-17 09:52:49 +0200

    Handle unauthorized request

--- lib/custom_devise_failure_app.rb
+++ lib/custom_devise_failure_app.rb
@@ -1,4 +1,18 @@
 class CustomDeviseFailureApp < Devise::FailureApp
+  def respond
+    if request.controller_class.to_s.start_with? 'Api::'
+      json_api_error_response
+    else
+      super
+    end
+  end
+
+  def json_api_error_response
+    self.status        = 401
+    self.content_type  = 'application/json'
+    self.response_body = { errors: [{ status: '401', title: i18n_message }]}.to_json
+  end
+
   def http_auth_body
     return super unless request_format == :json
 
