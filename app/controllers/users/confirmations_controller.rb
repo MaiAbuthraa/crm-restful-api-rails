4b7ff9f86f8551a387c87e614896a8926f0e6a64
MaiAbuthraa <mai.abuthraa@gmail.com>
2020-11-23 12:05:59 +0200

    Resend / Conformation

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,14 +1,12 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
-  before_action :set_resource, only: [:create]
-
   def create
     self.resource = resource_class.send_confirmation_instructions(resource_params)
     yield resource if block_given?
 
     if successfully_sent?(resource)
-      respond_with({}, :location => after_resending_confirmation_instructions_path_for(resource_name))
+      render json: {messages: [I18n.t('devise.confirmations.send_instructions')]}, status: :created
     else
-      respond_with(resource)
+      render json: {errors: resource.errors}, status: :unprocessable_entity
     end
   end
 
@@ -21,16 +19,5 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
     else
       render json: {errors: resource.errors},  status: :unprocessable_entity
     end
-  end
-
-  protected
-  # The path used after resending confirmation instructions.
-  def after_resending_confirmation_instructions_path_for(resource_name)
-    root_path
-  end
-
-  private
-  def set_resource
-
   end
 end
